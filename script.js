1a19470682bc4df9b3b6b43fd535b3663841c1f5
himanshu <r99.himanshu@gmail.com>
2024-06-27 12:29:45 +0530

    Fix - More Than One Decimal In A Number Bug Fixed

--- script.js
+++ script.js
@@ -5,16 +5,18 @@ const outputField = document.getElementById('outputField');
 const operators = ['+', '-', '*', '/', '%'];
 const digits = ['0', '1', '2', '3', '4', '5', '6', '7', '8', '9'];
 
-// To Count Brackets in Expression
+// To Count Brackets In Expression
 const bracketsCount = {
     '(': 0,
     ')': 0,
 }
 
-// Adding Event Listeners To All The Calculator Buttons
+// Adding Click Event Listeners To All The Calculator Buttons
 Array.from(buttons).forEach((element) =>
     element.addEventListener('click', () => {
-        // '=' Button : Evaluating Input Field Field Expression to Output Field
+        // Storing The Last Entered Token In Input Field
+        let lastToken = inputField.value.slice(-1);
+        // '=' Button : Evaluating Input Field Field Expression To Output Field
         if (element.innerHTML === '=') {
             try {
                 outputField.innerHTML = '= ' + eval(inputField.value);
@@ -32,14 +34,13 @@ Array.from(buttons).forEach((element) =>
         }
         // '←' Button : Back Button
         else if (element.innerHTML === '←') {
-            if (inputField.value.slice(-1) === '(' || inputField.value.slice(-1) === ')') {
-                bracketsCount[inputField.value.slice(-1)]--;
+            if (lastToken === '(' || lastToken === ')') {
+                bracketsCount[lastToken]--;
             }
             inputField.value = inputField.value.slice(0, -1);
         }
-        // '()' Button : Adding Brackets while validating Expression Syntax
+        // '()' Button : Adding Brackets While Validating Expression Syntax
         else if (element.innerHTML === '( )') {
-            const lastToken = inputField.value.slice(-1);
             if (bracketsCount['('] > bracketsCount[')'] || bracketsCount['('] === 0) {
                 if (bracketsCount['('] !== 0 && (digits.includes(lastToken) || lastToken === ')')) {
                     inputField.value += ')';
@@ -51,20 +52,34 @@ Array.from(buttons).forEach((element) =>
                 }
             }
         }
-        // Operator Buttons : Appending Operator while validating Expression Syntax
+        // Operator Buttons : Appending Operator While Validating Expression Syntax
         else if (operators.includes(element.innerHTML)) {
-            // If there is already an Operator just before
-            if (!(operators.includes(inputField.value.slice(-1))) && inputField.value.slice(-1) !== '.') {
+            // If There Is Already An Operator Just Before
+            if (!(operators.includes(lastToken)) && lastToken !== '.') {
                 inputField.value += element.innerHTML;
             }
         }
-        // Digits and '.' Button : Adding Digits and Dot Operator (while validating Expression Syntax)
+        // Digits And '.' Button : Adding Digits And Dot Operator (While Validating Expression Syntax)
         else {
+            // If Input Is Decimal
             if (element.innerHTML === '.') {
-                if (digits.includes(inputField.value.slice(-1))) {
-                    inputField.value += '.';
+                // Checking If There Is A Digit At The End Of The Input 
+                if (digits.includes(lastToken)) {
+                    // Checking For Invalid Syntaxes Like 5.3.2
+                    let inputFieldValue = inputField.value;
+
+                    let index = inputFieldValue.length - 1;
+                    while (index >= 0 && digits.includes(inputFieldValue[index])) {
+                        index--;
+                    }
+
+                    if (index === -1 || inputFieldValue[index] !== '.') {
+                        inputField.value += '.';
+                    }
+
                 }
             }
+            // If Input Is A Digit
             else {
                 inputField.value += element.innerHTML;
             }
